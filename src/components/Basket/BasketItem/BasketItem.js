ea16f7bd03b5be394143dd27c0a3aa178d20fe27
Sebastian Matysiak <sebastian.matysiak@hotmail.com>
2020-12-20 12:30:51 +0000

    Remove item from basket when qty decreases to 0

--- src/components/Basket/BasketItem/BasketItem.js
+++ src/components/Basket/BasketItem/BasketItem.js
@@ -1,22 +1,26 @@
 import React, { useState } from 'react';
-// import useGlobalContext from '../../../context';
+import { useGlobalContext } from '../../../context';
 
 import './BasketItem.css';
 
 const BasketItem = ({ item }) => {
-    // const { basket, setBasket, setBasketCount } = useGlobalContext();
+    const { basket, setBasket, setBasketCount } = useGlobalContext();
 
     const [quantity, setQuantity] = useState(item.quantity);
 
     //todo: 
     //1. when same product is added to the basket, merge quantity instead of adding another item
     //2. when using setBasket, use setBasketCount - this will eliminate a bit of repetetiveness
-    //3. if quantity is 0, just remove product from basket
-    const decreaseQuantity = () => {
+    //3. if quantity is 0, just remove product from basket - DONE
+    const decreaseQuantity = (id) => {
         let newQty = validateQuantity(quantity - 1);
-        setQuantity(newQty);
+        if (newQty === 0) {
+            removeItemFromBasket(id);
+        } else {
+            setQuantity(newQty);
+        }
     }
-    const increaseQuantity = () => {
+    const increaseQuantity = (id) => {
         let newQty = validateQuantity(quantity + 1);
         setQuantity(newQty);
     }
@@ -28,6 +32,14 @@ const BasketItem = ({ item }) => {
         return quantityToBe;
     }
 
+    const removeItemFromBasket = (productId) => {
+        const tempBasket = [...basket];
+        const itemToDeleteKey = basket.findIndex(item => item.id === productId);
+        tempBasket.splice(itemToDeleteKey, 1);
+
+        setBasket(tempBasket);
+    }
+
     return (
         <li className="basket__item basket-item">
             <div className="basket-item__info">
@@ -36,9 +48,9 @@ const BasketItem = ({ item }) => {
             </div>
             <div className="basket-item__qty">
                 <form className="basket-item__qty-form">
-                    <button type="button" onClick={decreaseQuantity}>-</button>
-                    <input type="number" name={`qty_${item.id}`} id={`qty_${item.id}`} value={quantity} />
-                    <button type="button" onClick={increaseQuantity}>+</button>
+                    <button type="button" onClick={() => decreaseQuantity(item.id)}>-</button>
+                    <input type="number" className="basket-item__qty-field" name={`qty_${item.id}`} id={`qty_${item.id}`} value={quantity} />
+                    <button type="button" onClick={() => increaseQuantity(item.id)}>+</button>
                 </form>
             </div>
         </li>
