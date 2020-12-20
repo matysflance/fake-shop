6fc62832e404fa58ca3abf45e3684e44fad8fbc4
Sebastian Matysiak <sebastian.matysiak@hotmail.com>
2020-12-20 17:59:17 +0000

    Extended functionality of changing quantities in the basket. Total basket count is now being updated whenever basket is updated. Increasing quantity is working too

--- src/components/Basket/BasketItem/BasketItem.js
+++ src/components/Basket/BasketItem/BasketItem.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useGlobalContext } from '../../../context';
 
 import './BasketItem.css';
@@ -9,20 +9,35 @@ const BasketItem = ({ item }) => {
     const [quantity, setQuantity] = useState(item.quantity);
 
     //todo: 
-    //1. when same product is added to the basket, merge quantity instead of adding another item
-    //2. when using setBasket, use setBasketCount - this will eliminate a bit of repetetiveness
-    //3. if quantity is 0, just remove product from basket - DONE
+    //1. when same product is added to the basket, merge quantity instead of adding another item - will work on next (not in this component though)
+
+    //2. when using setBasket, use setBasketCount - this will eliminate a bit of repetetiveness - DONE
+    useEffect(() => {
+        let totalBasketCount = 0;
+        basket.forEach(item => totalBasketCount += item.quantity);
+        setBasketCount(totalBasketCount);
+    }, [basket, setBasket])
+
     const decreaseQuantity = (id) => {
         let newQty = validateQuantity(quantity - 1);
+        //3. if quantity is 0, just remove product from basket - DONE
         if (newQty === 0) {
             removeItemFromBasket(id);
         } else {
+            const tempBasket = [...basket];
+            const itemToUpdateKey = basket.findIndex(item => item.id === id);
+            tempBasket[itemToUpdateKey].quantity = newQty;
             setQuantity(newQty);
+            setBasket(tempBasket);
         }
     }
     const increaseQuantity = (id) => {
         let newQty = validateQuantity(quantity + 1);
+        const tempBasket = [...basket];
+        const itemToUpdateKey = basket.findIndex(item => item.id === id);
+        tempBasket[itemToUpdateKey].quantity = newQty;
         setQuantity(newQty);
+        setBasket(tempBasket);
     }
 
     const validateQuantity = (quantityToBe) => {
