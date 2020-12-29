3ef9427d0bb8696add24c57dce19913538dcd3d7
Sebastian Matysiak <sebastian.matysiak@hotmail.com>
2020-12-29 15:40:55 +0000

    Made improvements for screen readers

--- src/components/Basket/BasketItem/BasketItem.js
+++ src/components/Basket/BasketItem/BasketItem.js
@@ -4,8 +4,10 @@ import { formatPrice } from '../../../util';
 
 import './BasketItem.css';
 
-const BasketItem = ({ item: {id, title, image, quantity, price} }) => {
-    const {increaseQuantity, decreaseQuantity, removeItemFromBasket} = useBasketContext();
+const calculateSubtotal = (price, quantity) => price * quantity;
+
+const BasketItem = ({ item: { id, title, image, quantity, price } }) => {
+    const { increaseQuantity, decreaseQuantity, removeItemFromBasket } = useBasketContext();
 
     const handleIncreaseQuantity = () => {
         increaseQuantity(id);
@@ -23,11 +25,17 @@ const BasketItem = ({ item: {id, title, image, quantity, price} }) => {
 
     return (
         <li className="basket__item basket-item">
-            <div className="basket-item__info">
-                <img src={image} alt={title} className="basket-item__image" />
-                {title} (£{formatPrice(price)})
+            <div className="basket-item__info-wrapper">
+                <div className="basket-item__info">
+                    <img src={image} alt={title} className="basket-item__image" />
+                    <p>{title}</p> (<span class="sr-only">Product price:</span>&pound;{formatPrice(price)})
+                </div>
+                <div className="basket-item__subtotal">
+                    Subtotal: &pound;{formatPrice(calculateSubtotal(price, quantity))}
+                </div>
             </div>
             <div className="basket-item__qty">
+                <span class="sr-only">Quantity:</span>
                 <form className="basket-item__qty-form">
                     <button type="button" onClick={handleDecreaseQuantity}>-</button>
                     <input type="number" className="basket-item__qty-field" name={`qty_${id}`} id={`qty_${id}`} value={quantity} />
--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -30,8 +30,9 @@ const Product = ({ product }) => {
                     {description}
                 </div>
                 <footer className="product__footer">
-                    <div className="product__price">£{formatPrice(price)}</div>
+                    <div className="product__price"><span class="sr-only">Product price:</span>&pound;{formatPrice(price)}</div>
                     <form className="product__form" onSubmit={(e) => handleAddToBasket(e)}>
+                        <span class="sr-only">Select quantity:</span>
                         <select id={`qty_${id}`} value={quantity} onChange={(e) => setQuantity(parseInt(e.target.value))}>
                             <option value="1">1</option>
                             <option value="2">2</option>
