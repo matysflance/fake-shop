cb91f94029fb8a77ab65cc60b97702b027cda3f0
Sebastian Matysiak <sebastian.matysiak@hotmail.com>
2020-12-31 14:50:54 +0000

    Modified api call function in order to stop mutating the string

--- src/api/index.js
+++ src/api/index.js
@@ -4,14 +4,8 @@ const PRODUCTS_URL = `${BASE_URL}/products`;
 const getLimitedProductsURL = (limit) => `${PRODUCTS_URL}?limit=${limit}`;
 
 export const fetchProducts = async (limitResults) => {
-    let productsURL = `${BASE_URL}/products`;
-
-    if (limitResults) {
-        productsURL = getLimitedProductsURL(limitResults);
-    }
-
     try {
-        const response = await fetch(productsURL);
+        const response = limitResults ? await fetch(getLimitedProductsURL(limitResults)) : await fetch(PRODUCTS_URL);
         return response.json();
     } catch (error) {
         return error;
