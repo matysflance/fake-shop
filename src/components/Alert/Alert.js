52b983fb25fce72ac51e72dcd456657fd2660157
Sebastian Matysiak <sebastian.matysiak@hotmail.com>
2021-01-02 22:51:06 +0000

    Added styled-components and changed Alert to be a styled componentl

--- src/components/Alert/Alert.js
+++ src/components/Alert/Alert.js
@@ -1,11 +1,37 @@
 import React from 'react';
+import styled from 'styled-components';
 
-import './Alert.css';
+const StyledAlert = styled.div`
+    position: fixed;
+    bottom: 5rem;
+    left: 50%;
+    transform: translateX(-50%);
+    z-index: 10;
+    padding: 1rem;
+    border-radius: 2px;
+    min-width: 20rem;
+    text-align: center;
+    font-weight: bold;
+    font-size: 1.5rem;
+    background-color: ${props => props.type === 'danger' ? 'rgb(196, 49, 12)' : 'rgb(12, 196, 12)'};
+    color: #fff;
 
-export const Alert = ({ type, message }) => {
+    @media (min-width: 576px) {
+        bottom: unset;
+        top: 5rem;
+        max-width: 50rem;
+    }
+
+    @media (min-width: 992px) {
+        top: 10rem;
+        max-width: 80rem;
+    }
+`;
+
+export const Alert = ({ message }) => {
     return (
-        <div className={`alert alert--${type}`} role='alert' aria-live='assertive'>
+        <StyledAlert role='alert' aria-live='assertive'>
             {message}
-        </div>
+        </StyledAlert>
     )
 }
