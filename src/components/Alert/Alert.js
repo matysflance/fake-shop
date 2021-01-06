059bea0a3027449a739d3e7f90baa354e39a2126
Sebastian Matysiak <sebastian.matysiak@hotmail.com>
2021-01-06 21:17:21 +0000

    Updated styling of alerts

--- src/components/Alert/Alert.js
+++ src/components/Alert/Alert.js
@@ -1,37 +1,11 @@
 import React from 'react';
-import styled from 'styled-components';
+import styles from './Alert.module.css';
+import clsx from 'clsx';
 
-const StyledAlert = styled.div`
-    position: fixed;
-    bottom: 5rem;
-    left: 50%;
-    transform: translateX(-50%);
-    z-index: 10;
-    padding: 1rem;
-    border-radius: 2px;
-    min-width: 20rem;
-    text-align: center;
-    font-weight: bold;
-    font-size: 1.5rem;
-    background-color: ${props => props.type === 'danger' ? 'rgb(196, 49, 12)' : 'rgb(12, 196, 12)'};
-    color: #fff;
-
-    @media (min-width: 576px) {
-        bottom: unset;
-        top: 5rem;
-        max-width: 50rem;
-    }
-
-    @media (min-width: 992px) {
-        top: 10rem;
-        max-width: 80rem;
-    }
-`;
-
-export const Alert = ({ message }) => {
+export const Alert = ({ message, type }) => {
     return (
-        <StyledAlert role='alert' aria-live='assertive'>
+        <div className={clsx(styles.alert, type === 'danger' ? styles.alertDanger : styles.alertSuccess)} role='alert' aria-live='assertive'>
             {message}
-        </StyledAlert>
+        </div>
     )
 }
